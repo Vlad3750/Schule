46462e4e9144314c1c19fc3e580478412d09902f
vladislav.neicovcen@student.htl-rankweil.at <vladislav.neicovcen@student.htl-rankweil.at>
2026-03-05 11:28:52 +0000

    Created Startseite.RowTemplate2, Backend and Startseite.RowTemplate1; Edited Startseite and settings

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -1,10 +1,10 @@
 from ._anvil_designer import StartseiteTemplate
 from anvil import *
+import anvil.server
 import plotly.graph_objects as go
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from plotly import graph_objects as go
 
 class Startseite(StartseiteTemplate):
   def __init__(self, **properties):
@@ -15,36 +15,28 @@ class Startseite(StartseiteTemplate):
     # Any code you write here will run before the form opens.
   # Plot some data
     self.diagramm.data = [
-      go.Scatter(
-        x = [1, 2, 3],
-        y = [3, 1, 6],
-        marker = dict(
-          color= 'rgb(0, 0, 0)'
-        )
+      go.Bar(
+        x = [2],
+        y = [5],
+#        marker = dict(
+#          color= 'rgb(212, 179, 60)'),
+        name = 'Lehrer'
       ),
       go.Bar(
-        x = [1, 2, 3],
-        y = [3, 1, 6],
-        marker = dict(
-          color= 'rgb(255, 224, 113)'),
-        name = 'Bar Chart Example'
+        x = [1],
+        y = [3],
+#        marker = dict(
+#          color= 'rgb(255, 224, 113)'),
+        name = 'Schüler'
       )
+      
     ]
 
     self.diagramm.layout = {
-      'title': {'text': 'Simple Example'},
-      'xaxis': {
-        'title': {'text': 'Time'}
-      }
+      'title': {'text': 'Diagramm'}
     }
-    
+      
     self.diagramm.layout.yaxis.title.text = 'Anzahl'
-    self.diagramm.layout.annotations = [{
-      'text': 'Simple annotation',
-      'x': 1,
-      'xref': 'x',
-      'y': 3.2,
-      'yref': 'y'
-    }]
+
     
       
